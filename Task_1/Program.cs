ae881551e31775158baf1173faa0021445b1bcdb
Julia <yulya_si@mail.ru>
2023-06-30 17:39:55 +0300

    Add Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Prompt(string message)
+{
+    System.Console.Write($"{message} ");
+    return int.Parse(Console.ReadLine());
+}
+
+int Reverse(int userNumber)
+{
+    int number = 0;
+    while (userNumber > 0)
+    {
+        int i = userNumber % 10;
+        number = number * 10 + i;
+        userNumber /= 10;
+    }
+    return number;
+}
+
+bool ValidatePalyndrom(int valOne, int valTwo)
+{
+    return valOne == valTwo;
+}
+
+int userNumber = Prompt("Введите число");
+int palyndrom = Reverse(userNumber);
+if (ValidatePalyndrom(userNumber, palyndrom)) System.Console.WriteLine($"Число {userNumber} является палиндромом");
+else System.Console.WriteLine($"Число {userNumber} не палиндром");
